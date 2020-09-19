dce4ea2c19b7853354a3366768bf94d3ce32d665
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-09-19 12:10:22 +0200

    Fix(docker): Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-JSON_STRING='window.configs = { \
-  "VUE_APP_API_ENDPOINT":"'"${VUE_APP_API_ENDPOINT}"'", \
-}'sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+JSON_STRING='window.configs = { "VUE_APP_API_ENDPOINT":"'"${VUE_APP_API_ENDPOINT}"'", }'
+sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
 
 exec "$@"
